d7466496cc6822ea580d2f2d28f3e531cd32d5c5
alexander16108 <alexandermayowa05@gmail.com>
2021-07-01 11:37:22 +0100

    Fix Linters errors

--- js/script.js
+++ js/script.js
@@ -1,26 +1,11 @@
 // popup-menu mobile version
-const headList = document.querySelector('.hide_pop');
-const openMenu = document.querySelector('.open_pop');
-const closeMenu = document.querySelectorAll('.close_pop');
-const closeBtn = document.querySelector('.close-menu')
+const headList = document.querySelector('.nav-links');
+const openMenu = document.querySelector('.openPop');
+const closeBtn = document.querySelector('.closeMenu');
 
-
-
-
-openMenu.addEventListener('click', show);
-// closeMenu.addEventListener('click', close);
-closeBtn.addEventListener('click', close)
-
-/**  adding css style to the elements
- *   for opening
- */
-function show() {
-  headList.style.display = 'flex';
-  headList.style.top = '0';
-}
-/** adding css style to the elements
- *  for closing
- */
-function close() {
-  closeBtn.style.top = '-100%';
-}
+openMenu.addEventListener('click', () => {
+  headList.classList.add('show');
+});
+closeBtn.addEventListener('click', () => {
+  headList.classList.remove('show');
+});
